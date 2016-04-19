47da3709fd94547f98c3a150cba52cf91c485c1f
JE- <Jonathan.erez@gmail.com>
2016-04-19 15:01:35 -0400

    ok

--- app.py
+++ app.py
@@ -11,12 +11,5 @@ def root():
     else:
         return render_template('userinfo.html')
 
-@app.route('/trying',methods=['GET','POST'])
-def root():
-    if request.method == 'GET':
-        return render_template('trying.html')
-    else:
-        return render_template('userinfo.html')
-
 if __name__ == "__main__":
     app.run(debug=True)
