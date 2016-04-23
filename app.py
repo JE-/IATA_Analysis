e14e3cfaff8326b3c5ec0d39961a919b750033cc
JE- <Jonathan.erez@gmail.com>
2016-04-23 17:42:08 -0400

    minor changes

--- app.py
+++ app.py
@@ -2,6 +2,13 @@ from flask import Flask,render_template,request,redirect
 import os
 import datetime
 
+
+#### Flask
+
+class ReviewForm(Form):
+    moviereview = TextAreaField('',
+                                [validators.DataRequired(),
+                                validators.length(min=15)])
 app = Flask(__name__)
 
 @app.route('/',methods=['GET','POST'])
