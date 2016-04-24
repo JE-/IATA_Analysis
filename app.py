b6182504528ac4d8fa13769253d04231bf68bae8
JE- <Jonathan.erez@gmail.com>
2016-04-24 14:22:18 -0400

    minor changes

--- app.py
+++ app.py
@@ -18,14 +18,15 @@ cur_dir = os.path.dirname(__file__)
 clf = pickle.load(open(os.path.join(cur_dir,
                   'pickled_objects',
                   'classifier.pkl'), 'rb'))
-#db = os.path.join(cur_dir, 'reviews.sqlite')
-#
-# def classify(document):
-#     label = {0: 'negative', 1: 'positive'}
-#     X = vect.transform([document])
-#     y = clf.predict(X)[0]
-#     proba = np.max(clf.predict_proba(X))
-#     return label[y], proba
+
+db = os.path.join(cur_dir, 'reviews.sqlite')
+
+def classify(document):
+    label = {0: 'negative', 1: 'positive'}
+    X = vect.transform([document])
+    y = clf.predict(X)[0]
+    proba = np.max(clf.predict_proba(X))
+    return label[y], proba
 #
 # def train(document, y):
 #     X = vect.transform([document])
