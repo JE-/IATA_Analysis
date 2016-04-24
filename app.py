28723cca139b56c2c06dccf8df61f37797f29ebb
JE- <Jonathan.erez@gmail.com>
2016-04-24 13:32:40 -0400

    minor changes

--- app.py
+++ app.py
@@ -12,10 +12,10 @@ import numpy as np
 #from vectorizer import vect
 
 ######### Preparing the Classifier
-cur_dir = os.path.dirname(__file__)
-clf = pickle.load(open(os.path.join(cur_dir,
-                 'pickled_objects',
-                 'classifier.pkl'), 'rb'))
+# cur_dir = os.path.dirname(__file__)
+# clf = pickle.load(open(os.path.join(cur_dir,
+#                  'pickled_objects',
+#                  'classifier.pkl'), 'rb'))
 #db = os.path.join(cur_dir, 'reviews.sqlite')
 #
 # def classify(document):
