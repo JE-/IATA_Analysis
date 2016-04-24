9a5a6c5a6840026fd93161e2d406d1e73fa3f2ee
JE- <Jonathan.erez@gmail.com>
2016-04-24 12:39:34 -0400

    minor changes

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ import numpy as np
 #from vectorizer import vect
 
 ######## Preparing the Classifier
-# cur_dir = os.path.dirname(__file__)
+cur_dir = os.path.dirname(__file__)
 # clf = pickle.load(open(os.path.join(cur_dir,
 #                  'pkl_objects',
 #                  'classifier.pkl'), 'rb'))
