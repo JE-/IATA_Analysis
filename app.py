f5c987d37f10af860ee35c2200639ad2ba104502
JE- <Jonathan.erez@gmail.com>
2016-04-25 08:33:23 -0400

    minor changes

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 @app.route('/',methods=['GET','POST'])
 def root():
     if request.method == 'GET':
-        return render_template('index.html', form=form)
+        return render_template('index.html')
     else:
         return render_template('userinfo.html')
 
