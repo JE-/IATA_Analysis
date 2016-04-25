572c325d17edd4f80c1baac5eaf109ad3757ce77
JE- <Jonathan.erez@gmail.com>
2016-04-25 08:27:23 -0400

    minor changes

--- app.py
+++ app.py
@@ -7,40 +7,7 @@ import pickle
 import sqlite3
 import numpy as np
 
-from vectorizer import vect
-
-##### Preparing the Classifier
-cur_dir = os.path.dirname(__file__)
-clf = pickle.load(open(os.path.join(cur_dir,
-                  'pickled_objects',
-                  'classifier.pkl'), 'rb'))
-
-db = os.path.join(cur_dir, 'reviews.sqlite')
-
-def classify(document):
-    label = {0: 'negative', 1: 'positive'}
-    X = vect.transform([document])
-    y = clf.predict(X)[0]
-    proba = np.max(clf.predict_proba(X))
-    return label[y], proba
-
-def train(document, y):
-    X = vect.transform([document])
-    clf.partial_fit(X, [y])
-
-def sqlite_entry(path, document, y):
-    conn = sqlite3.connect(path)
-    c = conn.cursor()
-    c.execute("INSERT INTO review_db (review, sentiment, date)"\
-    " VALUES (?, ?, DATETIME('now'))", (document, y))
-    conn.commit()
-    conn.close()
-
 #### Flask
-class ReviewForm(Form):
-    moviereview = TextAreaField('',
-                                [validators.DataRequired(),
-                                validators.length(min=15)])
 app = Flask(__name__)
 
 @app.route('/',methods=['GET','POST'])
@@ -51,18 +18,5 @@ def root():
     else:
         return render_template('userinfo.html')
 
-@app.route('/results', methods=['POST'])
-def results():
-    form = ReviewForm(request.form)
-    if request.method == 'POST' and form.validate():
-        review = request.form['moviereview']
-        y, proba = classify(review)
-        return render_template('results.html',
-                                content=review,
-                                prediction=y,
-                                probability=round(proba*100, 2))
-    return render_template('index.html', form=form)
-
 if __name__ == "__main__":
-    app.debug = True
     app.run(debug=True)
